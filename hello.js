f494b908ee90acb0d9f7d86ac0405b2c50864448
gudrb6330 <36982486+gudrb6330@users.noreply.github.com>
2018-03-27 14:41:48 +0900

    ㅎㅎ

--- hello.js
+++ hello.js
@@ -1,11 +1,23 @@
 var http = require('http');
-
-function requestCallbackFunction (req, res) {
-  // if(req.url === '/favicon.ico') {
+/*
+function requestCallbackFunction(rep, res) {
+// if(req.url === '/favicon.ico') {
   //   console.log('Favicon was requested');
   // } else {
     console.log('hello');
   // }
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World!');
+}
+
+*/
+ver server = http.createServer(
+  function(rep, res) {
+ if(req.url === '/favicon.ico') {
+     console.log('Favicon was requested');
+   } else {
+    console.log('hello');
+   }
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World!');
 }
